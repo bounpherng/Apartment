70909350def4b3b6603f7365c04658af9eb357c0
boun <143073009+bounpherng@users.noreply.github.com>
2025-10-29 08:56:04 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,20 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = 'pwa-shell-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('pwa-cache').then((cache) => {
-      return cache.addAll(['./']);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
